0eddaef55dd60b1190062490d13d0da5816e366a
Aya <ayabonga@projectcodex.co>
2017-10-19 23:08:25 +0200

    Fix: remove sensitve data

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -10,7 +10,7 @@
 (defn -main
   "I am the three eyed price man :)"
   [& args]
-  (def productSelection [{:title 'sim-family' :selected true} {:title 'sim-employee' :selected false} {:title 'sim-cashback' :selected true}])
+  (def productSelection [{:title 'Family' :selected true} {:title 'Household' :selected false} {:title 'Remu' :selected true}])
   (def selectedProducts (filterSelectedProducts productSelection))
   (println "")
   (println "")
@@ -19,21 +19,3 @@
   (println "")
   (println "A product is not a slave")
   (println selectedProducts)
-
-
-
-
-  ;;(def premium  (getPremium selectedProducts))
-  ;;(def ropPremium (- premiumWithRop premium)
-  ;;( if (pricingConfig.withRop)
-  ;;     (def ropCost ropPremium)
-  ;;     (def ropCost  0)
-  ;;)
-  ;;(def ropPrecentage ROP_PERCENTAGE)
-  ;;(def cost (+ premium ropCost)
-  ;;(def discountAmount ( getDiscount cost pricingConfig.discountRate)
-  ;;(def finalCost (- cost  discountAmount)
-  ;;(def discountedPremiumNoRop (- premium  (getDiscount premium pricingConfig.discountRate)))
-  ;;(def ropReturn (getRopReturn discountedPremiumNoRop ROP_PERCENTAGE)
-  ;;(def ropTotalPremium (totalPremiumOverFiveYears ropPremium))
-  ;;(def isAffordable (getAffordability income finalCost)
