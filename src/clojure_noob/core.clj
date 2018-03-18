d611fd9d4518bdd18452ac1eadbfb027ec542a62
Aya <ayabonga@projectcodex.co>
2018-03-18 14:40:07 +0200

    Introducedoggo tutorial

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -7,15 +7,44 @@
 
 (defn filterSelectedProducts [products](filter (fn[p](get p :selected)) products))
 
+(defn bothPass [conditionFunc1 conditionFunc2 obj](and (conditionFunc1 obj) (conditionFunc2 obj)))
+
 (defn -main
-  "I am the three eyed price man :)"
+  "I am the three eyed price man"
   [& args]
-  (def productSelection [{:title 'Family' :selected true} {:title 'Household' :selected false} {:title 'Remu' :selected true}])
-  (def selectedProducts (filterSelectedProducts productSelection))
-  (println "")
-  (println "")
-  (println "The products are coming ")
-  (println productSelection))
-  (println "")
-  (println "A product is not a slave")
-  (println selectedProducts)
+  
+  (defn isTeeny [doggo](= (get doggo :size) "teeny"))
+
+  (defn isDoggor [doggo](= (get doggo :type) "DOGGOR"))
+
+  (defn isGender [doggo](if (= (get doggo :gender) "F") "She" "He"))
+
+  (def doggos [
+    {:gender "F" :name "Missy" :type "floofer" :size "teeny" }
+    {:gender "F" :name "Billy" :type "pupper" :size "mega-pupperous"}
+    {:gender "M" :name "Mixie" :type "DOGGOR" :size "teeny"}
+    {:gender "F" :name "Boxer" :type "Huge Borker" :size "Mambo Jambo"}
+    {:gender "M" :name "Alice" :type "Doge" :size "miny"}
+    {:gender "F" :name "Vulcan" :type "DOGGOR" :size "Alpha"}
+    {:gender "M" :name "Mr beans" :type "Huge Borker" :size "Alpha"}
+    {:gender "F" :name "Scentwell" :type "DOGGOR" :size "teeny"}
+  ])
+
+  (defn describe [doggo](str "a" (get doggo :size) (get doggo :type)))
+
+
+  ;;;;;;;;;;; Content
+  (defn greet [doggo](str "Meet" (get doggo :name)))
+
+  (defn specify [doggo](str (isGender doggo) "is"))
+
+  (defn tellAbout [doggo]((greet doggo)(specify doggo)(describe doggo)))
+
+
+  ;;;;;;;;;;;; Output
+  (println "---")
+  (def stories (map tellAbout doggos))
+  (print stories)
+  (println "---")
+
+)
